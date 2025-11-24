c229c6f4dce8a6fc2f283c2c56dd0b8e4fdd022f
Mahen Abeywickrama <mahenabeywickrema0@gmail.com>
2025-11-24 11:47:45 +0530

    first commit

---
+++ src/services/api.ts
@@ -0,0 +1,61 @@
+// axiosConfig.ts
+// apiService.ts
+// api.ts
+import axios, { AxiosError } from "axios"
+import { refreshTokens } from "./auth"
+
+const api = axios.create({
+  baseURL: "https://smart-blog-be-six.vercel.app/api/v1"
+})
+
+const PUBLIC_ENDPOINTS = ["/auth/login", "/auth/register"]
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken")
+  const isPublic = PUBLIC_ENDPOINTS.some
+  ((url) => config.url?.includes(url))
+
+  if (token && !isPublic) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  async (err: AxiosError) => {
+    const originalRequest: any = err.config
+
+    const isPublic = PUBLIC_ENDPOINTS.some((url) =>
+      originalRequest.url?.includes(url)
+    )
+
+    if (err.response?.status === 401 && !isPublic && !originalRequest._retry) {
+      originalRequest._retry = true
+      try {
+        const refreshToken = localStorage.getItem("refreshToken")
+        if (!refreshToken) {
+          throw new Error("No refresh token vailable")
+        }
+        const res = await refreshTokens(refreshToken)
+        localStorage.setItem("accessToken", res.accessToken)
+
+        originalRequest.headers.Authorization = `Bearer ${res.accessToken}`
+
+        return axios(originalRequest)
+      } catch (error) {
+        localStorage.removeItem("accessToken")
+        localStorage.removeItem("refreshToken")
+        window.location.href = "/login"
+        console.error(error)
+        return Promise.reject(error)
+      }
+    }
+    return Promise.reject(err)
+  }
+)
+
+export default api
